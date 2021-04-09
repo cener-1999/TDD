461d9839cd9e86941218ddc7477ca90636adadd2
unknown <1065802928@qq.com>
2021-04-09 18:29:59 +0800

    Basic view now return minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,18 +1,28 @@
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page #(2)
+from django.http import HttpRequest
+
+from lists.views import home_page 
 
 # Create your tests here.
 
 class HomePageTest(TestCase):
     
     def test_root_url_resolve_to_home_page_view(self):
-        found = resolve('/') #(1)
-        self.assertEqual(found.func,home_page) #(1)
+        found = resolve('/') 
+        self.assertEqual(found.func,home_page) 
         
+    def test_home_page_returns_correct_html(self):
+        request =HttpRequest() #1
+        response = home_page(request) #2
+        html = response.content.decode('utf8')#3
+        self.assertTrue(html.startswith('<html>'))#4
+        self.assertIn('<title>To-Do list</title>',html)#5
+        self.assertTrue(html.endswith('</html>'))#4
+
 
 '''
-这些代码干了些什么？  
+这些代码干了些什么？ (第一版) 
 resolve是Django用来解析URL的function，找到URL应该映射到哪个view。 
 我们测试这个解析，当用“/”调用时，网站的根目录应该找到一个名为home_page的function。
 '''
