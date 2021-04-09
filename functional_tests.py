9dcb7e5d9cbf5bcfe982e4c1f7f3ddfbcaeba768
unknown <1065802928@qq.com>
2021-04-09 18:14:42 +0800

    First unit test and url mapping,dummy view

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,40 @@
-from selenium import webdriver  #(1)从selenium引入webdriver
+from selenium import webdriver  
+import unittest
 
-browser = webdriver.Firefox()  #(2)启动一个selenium“webdriver”去弹出一个Firefox窗口
-browser.get('http://localhost:8000')  #(3)用它打开本地网页
+class NewVisitorTest(unittest.TestCase): #(1)
 
-assert 'Django' in brower.title  #(4)这是测试断言，检查在网页Title中有没有“Django”这个词
+    def setUp(self):#(3)
+        self.browser = webdriver.Firefox() 
+
+    def tearDown(self):#(3)
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):#(2)
+        #Edith has heard about a cool new online to-do app.She goes
+        #to check out its homepage
+        self.browser.get('http://localhost:8000')
+        
+        #She notices the page title and header mention to-do lists
+        self.assertIn('To-Do',self.browser.title)#(4)
+        self.fail('Finish the test！')#(5)
+        
+if __name__ =='__main__': #(6)
+    unittest.main(warnings='ignore')#(7)
+
+'''
+(1)把测试写成类，它继承unittest.TestCase；
+(2)测试的主体是一个名为test_can_start_a_list_and_retrieve_it_later的方法。
+以test开头的任何方法都是测试方法，将由测试运行器运行。
+每个类可以有多个test方法。我们需要给每个测试方法的起一个描述性名称。
+看到名称，就知道测试什么。
+(3)setUp和tearDown是在每次测试之前和之后运行的特殊方法。使用它们来启动和停止我们的浏览器 
+注意它们有点像try / except，即使在测试期间出现错误，tearDown也会运行。
+没有多余的Firefox窗口躺在桌面了
+(4)使用self.assertIn代替断言。unittest提供了许多像这样的辅助功能来进行测试，
+比如assertEqual，assertTrue，assertFalse等等。您可以在unittest文档中找到更多信息。
+(5)self.fail无论如何都会失败，产生错误信息。我们用它作为完成测试的提醒。
+(6)if __name__ ==‘__ main__’子句，就相当于是 Python 模拟的程序入口。 
+我们调用unittest.main（）方法，它启动unittest测试运行器，
+它将自动在文件中寻找测试类和方法并运行它们。
+(7)warnings ='ignore’防止了在撰写本文时爆出多余的ResourceWarning信息。
+'''
--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,18 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page #(2)
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
     
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+    def test_root_url_resolve_to_home_page_view(self):
+        found = resolve('/') #(1)
+        self.assertEqual(found.func,home_page) #(1)
         
+
+'''
+这些代码干了些什么？  
+resolve是Django用来解析URL的function，找到URL应该映射到哪个view。 
+我们测试这个解析，当用“/”调用时，网站的根目录应该找到一个名为home_page的function。
+'''
