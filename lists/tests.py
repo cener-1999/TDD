d86bda3102955c4d7142aba07e7f7317a29001a3
unknown <1065802928@qq.com>
2021-05-07 13:33:06 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page 
+from lists.models import Item
 
 # Create your tests here.
 
@@ -24,8 +25,16 @@ class HomePageTest(TestCase):
         expected_html =render_to_string('home.html')
         self.assertEqual(html.expected_html)
     '''
+    '''
     def test_uses_home_template(self):
         response=self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
+        '''
+       
+       
+    def test_can_save_a_POST_request(self):
+        response =self.client.post('/',data={'item_text':'A new list item'})
+        self.assertIn('A new list item',response.content.decode())
         self.assertTemplateUsed(response,'home.html')
         
     '''
@@ -44,3 +53,23 @@ class HomePageTest(TestCase):
 resolve是Django用来解析URL的function，找到URL应该映射到哪个view。 
 我们测试这个解析，当用“/”调用时，网站的根目录应该找到一个名为home_page的function。
 '''
+
+class ItemModelTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        first_item =Item()
+        first_item.text='The first (ever) list item'
+        first_item.save()
+        
+        second_item=Item()
+        second_item.text='Item the second'
+        second_item.save()
+        
+        saved_items=Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+        
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(second_saved_item.text,'Item the second')
+        
